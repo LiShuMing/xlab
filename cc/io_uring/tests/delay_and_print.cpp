6fa51d64a9a0c11db94a91fecb0ce8cb2ed0ae0d
shuming.li <ming.moriarty@gmail.com>
2024-11-04 10:00:08 +0800

    add io_uring
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>

---
+++ cc/io_uring/tests/delay_and_print.cpp
@@ -0,0 +1,34 @@
+#include <fmt/chrono.h> // https://github.com/fmtlib/fmt
+
+#include <chrono>
+#include <liburing/io_service.hpp>
+
+int main() {
+    using uio::io_service;
+    using uio::task;
+    using uio::panic_on_err;
+    using uio::dur2ts;
+
+    io_service service;
+
+    service.run([](io_service& service) -> task<> {
+        auto delayAndPrint = [&](int second, uint8_t iflags = 0) -> task<> {
+            auto ts = dur2ts(std::chrono::seconds(second));
+            co_await service.timeout(&ts, iflags) | panic_on_err("timeout", false);
+            fmt::print("{:%T}: delayed {}s\n", std::chrono::system_clock::now().time_since_epoch(),
+                       second);
+        };
+
+        fmt::print("in sequence start\n");
+        co_await delayAndPrint(1);
+        co_await delayAndPrint(2);
+        co_await delayAndPrint(3);
+        fmt::print("in sequence end, should wait 6s\n\n");
+
+        fmt::print("io link start\n");
+        delayAndPrint(1, IOSQE_IO_HARDLINK);
+        delayAndPrint(2, IOSQE_IO_HARDLINK);
+        co_await delayAndPrint(3);
+        fmt::print("io link end, should wait 6s\n");
+    }(service));
+}
---
+++ cc/io_uring/tests/ping_pong.cpp
@@ -0,0 +1,81 @@
+#include <fmt/color.h>
+#include <fmt/core.h>
+
+#include <liburing/io_service.hpp>
+#include <string_view>
+
+auto ping(uio::io_service& service, int read_fd, int write_fd) -> uio::task<> {
+    std::string_view msg = "ping!";
+    std::string_view expected = "pong!";
+    std::array<char, 64> buffer;
+
+    for (int i = 0; i < 20; i++) {
+        int count = co_await service.read(read_fd, buffer.data(), buffer.size(), 0) |
+                    uio::panic_on_err("ping: Unable to read from read_fd", false);
+
+        auto recieved = std::string_view(buffer.data(), count);
+
+        fmt::print(fg(fmt::color::cyan) | fmt::emphasis::bold, "ping: Recieved {}\n", recieved);
+        if (recieved != expected) uio::panic("Unexpected message", 0);
+
+        co_await service.write(write_fd, msg.data(), msg.size(), 0) |
+                uio::panic_on_err("ping: Unable to write to write_fd", false);
+    }
+
+    co_await service.close(write_fd) | uio::panic_on_err("ping: Unable to close write_fd", false);
+
+    // Check for EOF before exiting
+    if (0 != co_await service.read(read_fd, buffer.data(), buffer.size(), 0)) {
+        throw std::runtime_error("pong: Pipe not at EOF like expected");
+    }
+
+    co_await service.close(read_fd) | uio::panic_on_err("ping: Unable to close read_fd", false);
+}
+
+auto pong(uio::io_service& service, int read_fd, int write_fd) -> uio::task<> {
+    std::string_view msg = "pong!";
+    std::string_view expected = "ping!";
+    std::array<char, 64> buffer;
+
+    for (int i = 0; i < 20; i++) {
+        co_await service.write(write_fd, msg.data(), msg.size(), 0) |
+                uio::panic_on_err("pong: Unable to write to write_fd", false);
+
+        int count = co_await service.read(read_fd, buffer.data(), buffer.size(), 0) |
+                    uio::panic_on_err("pong: Unable to read from read_fd", false);
+
+        auto recieved = std::string_view(buffer.data(), count);
+
+        fmt::print(fg(fmt::color::magenta) | fmt::emphasis::bold, "pong: Recieved {}\n", recieved);
+        if (recieved != expected) uio::panic("Unexpected message", 0);
+    }
+
+    co_await service.close(write_fd) | uio::panic_on_err("pong: Unable to close write_fd", false);
+
+    // Check for EOF before exiting
+    if (0 != co_await service.read(read_fd, buffer.data(), buffer.size(), 0)) {
+        throw std::runtime_error("pong: Pipe not at EOF like expected");
+    }
+
+    co_await service.close(read_fd) | uio::panic_on_err("pong: Unable to close read_fd", false);
+}
+
+int main() {
+    using uio::io_service;
+    using uio::task;
+
+    io_service io;
+
+    std::array<int, 2> p1;
+    std::array<int, 2> p2;
+    pipe(p1.data()) | uio::panic_on_err("Unable to open pipe", true);
+    pipe(p2.data()) | uio::panic_on_err("Unable to open pipe", true);
+
+    // ping reads from p1 and writes to p2
+    auto t1 = ping(io, p1[0], p2[1]);
+    // pong writes to p1 and reads from p2
+    auto t2 = pong(io, p2[0], p1[1]);
+
+    io.run(t1);
+    io.run(t2);
+}
