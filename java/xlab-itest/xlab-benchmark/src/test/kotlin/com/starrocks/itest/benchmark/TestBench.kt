6b0f6d85d2b9bc3dddc15ceb4917e18ac48b845f
shuming.li <ming.moriarty@gmail.com>
2024-12-13 17:37:10 +0800

    Add more java modules
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>

--- java/xlab-itest/xlab-benchmark/src/test/kotlin/com/starrocks/itest/benchmark/TestBench.kt
+++ java/xlab-itest/xlab-benchmark/src/test/kotlin/com/starrocks/itest/benchmark/TestBench.kt
@@ -1,4 +1,5 @@
 package com.starrocks.itest.benchmark
 
 class TestBench {
+
 }
---
+++ java/xlab-itest/xlab-itest/src/test/kotlin/com/starrocks/itest/MVWithMultiPartitionSuite.kt
@@ -0,0 +1,11 @@
+package com.starrocks.itest
+
+import com.starrocks.itest.framework.MVSuite
+import kotlin.test.Test
+
+class MVWithMultiPartitionSuite : MVSuite() {
+    @Test
+    fun testMVWithMultiColumns() {
+
+    }
+}
