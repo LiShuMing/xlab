abc717cfc1b65c0e2f9d8349c0c04428190b454f
shuming.li <ming.moriarty@gmail.com>
2024-12-16 19:40:26 +0800

    add more tests
    Signed-off-by: shuming.li <ming.moriarty@gmail.com>

--- python/pylab/test/test_sort.py
+++ python/pylab/test/test_sort.py
@@ -1,4 +1,7 @@
 import pytest
+from datetime import datetime, timedelta
+import random
+
 
 def test_sort_1():
     print("start test_sort_1")
@@ -68,3 +71,36 @@ def test_handle_files():
     for i in range(1, 23):
         f_name = f_dir + "/q" + str(i) + ".sql"
         handle_files(f_name)
+
+def generate_data():
+    data = []
+    start_date = datetime(2023, 1, 1)  # 数据起始日期
+    end_date = datetime(2023, 12, 31)  # 数据结束日期
+
+    for i in range(1, 10):  # 生成10万条数据
+        ship_date = start_date + timedelta(days=random.randint(0, 364))
+        commit_date = ship_date + timedelta(days=random.randint(1, 10))
+        receipt_date = commit_date + timedelta(days=random.randint(1, 5))
+        data.append((
+            random.randint(1, 1000000),   # l_orderkey
+            random.randint(1, 100000),    # l_partkey
+            random.randint(1, 50000),     # l_suppkey
+            random.randint(1, 10),        # l_linenumber
+            str(round(random.uniform(1, 100), 2)),  # l_quantity
+            str(round(random.uniform(10, 1000), 2)),  # l_extendedprice
+            str(round(random.uniform(0, 0.1), 2)),   # l_discount
+            str(round(random.uniform(0, 0.2), 2)),   # l_tax
+            random.choice(['A', 'B', 'C']),     # l_returnflag
+            random.choice(['O', 'F']),          # l_linestatus
+            ship_date,                          # l_shipdate
+            commit_date,                        # l_commitdate
+            receipt_date,                       # l_receiptdate
+            random.choice(['DELIVER IN PERSON', 'COLLECT FROM STORE']),  # l_shipinstruct
+            random.choice(['AIR', 'RAIL', 'TRUCK']),                    # l_shipmode
+            "Generated comment {}".format(i)    # l_comment
+        ))
+    return data
+
+def test_generate_data():
+    data = generate_data()
+    print(data)
---
+++ python/pylab/tools/pyspark/test_spark.py
@@ -0,0 +1,85 @@
+
+import os
+
+from pyspark import SparkConf
+from pyspark.sql import SparkSession, DataFrame
+import pyspark.sql.functions as F
+from pyspark.sql import SparkSession
+
+catalog_name = "local"
+# warehouse_path = f"s3://{bucket_name}/iceberg"
+warehouse_path = f"./tmp/iceberg"
+
+spark = SparkSession.builder \
+    .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions") \
+    .config(f"spark.sql.catalog.{catalog_name}", "org.apache.iceberg.spark.SparkCatalog") \
+    .config(f"spark.sql.catalog.{catalog_name}.type", "hive") \
+    .config(f"spark.sql.catalog.{catalog_name}.uri", "thrift://172.26.194.238:9083") \
+    .config("spark.jars", "/root/work/spark-3.5.1-bin-hadoop3/jars/iceberg-spark-runtime-3.5_2.12-1.5.1.jar") \
+    .config("spark.jars", "/root/work/spark-3.5.1-bin-hadoop3/jars/iceberg-spark-runtime-3.5_2.12-1.5.1.jar") \
+    .getOrCreate()
+    # .config(f"spark.sql.catalog.{catalog_name}.warehouse", warehouse_path) \
+    # .config(f"spark.sql.catalog.{catalog_name}.catalog-impl", "org.apache.iceberg.aws.glue.GlueCatalog") \
+    # .config(f"spark.sql.catalog.{catalog_name}.io-impl", "org.apache.iceberg.aws.s3.S3FileIO") \
+
+spark.sparkContext.setLogLevel('INFO')
+
+spark.sql("use local.lism")
+spark.sql("show tables").show(truncate=False)
+
+# Create an empty Iceberg table
+query = f"""
+CREATE TABLE {catalog_name}.lism.lineitem_days2 (
+                          l_orderkey    BIGINT,
+                          l_partkey     INT,
+                          l_suppkey     INT,
+                          l_linenumber  INT,
+                          l_quantity    DECIMAL(15, 2),
+                          l_extendedprice  DECIMAL(15, 2),
+                          l_discount    DECIMAL(15, 2),
+                          l_tax         DECIMAL(15, 2),
+                          l_returnflag  VARCHAR(1),
+                          l_linestatus  VARCHAR(1),
+                          l_shipdate    TIMESTAMP,
+                          l_commitdate  TIMESTAMP,
+                          l_receiptdate TIMESTAMP,
+                          l_shipinstruct VARCHAR(25),
+                          l_shipmode     VARCHAR(10),
+                          l_comment      VARCHAR(44)
+) USING ICEBERG
+PARTITIONED BY (l_returnflag, l_linestatus, days(l_shipdate));
+"""
+spark.sql(query)
+
+# spark.sql("use local.lism")
+# spark.sql("show tables").show(truncate=False)
+
+# query_select_files = f"select file_path from {catalog_name}.lism.demo_target_iceberg_add_files.files"
+# spark.sql(query_select_files).show(10, truncate=False)
+
+# query_select_snapshots = f"select snapshot_id, manifest_list from {catalog_name}.lism.demo_target_iceberg_add_files.snapshots"
+# spark.sql(query_select_snapshots).show(10, truncate=False)
+
+# # Run the add_files procedure
+# query = f"""
+# call {catalog_name}.system.add_files(
+#     table => 'lism.demo_target_iceberg_add_files',
+#     source_table => 'lism.demo_source_parquet'
+# )
+# """
+# spark.sql(query).show(truncate=False)
+
+# spark.sql(query_select_files).show(10, truncate=False)
+# spark.sql(query_select_snapshots).show(10, truncate=False)
+
+# # add one record
+# query = f"""
+# insert into {catalog_name}.lism.demo_target_iceberg_add_files
+#     select * from lism.demo_source_parquet LIMIT 1
+# """
+# spark.sql(query)
+
+# spark.sql(query_select_files).show(10, truncate=False)
+# spark.sql(query_select_snapshots).show(10, truncate=False)
+
+spark.stop()
