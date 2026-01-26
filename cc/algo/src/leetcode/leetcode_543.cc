47e36390bb6ea7fe6cede0b7cfcc45f727d7cdbb
shuming.li <ming.moriarty@gmail.com>
2026-01-26 22:12:20 +0800

    add more codes

--- cc/algo/src/leetcode/leetcode_543.cc
+++ cc/algo/src/leetcode/leetcode_543.cc
@@ -2,22 +2,24 @@
 
 /**
  * 543. Diameter of Binary Tree
- * 
- * Given a binary tree, you need to compute the length of the diameter of the tree. 
- * The diameter of a binary tree is the length of the longest path between any two nodes in a tree. 
+ *
+ * Given a binary tree, you need to compute the length of the diameter of the tree.
+ * The diameter of a binary tree is the length of the longest path between any two nodes in a tree.
  * This path may or may not pass through the root.
- * 
+ *
  * Example:
  * Given a binary tree
  *           1
  *          / \
  *         2   3
- * The diameter of the tree is 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
+ *        /
+ *       4
+ * The diameter of the tree is 3, which is the path [4,2,1,3] (4 edges: 4->2->1->3)
  */
- class Solution {
-    private:
+class Solution {
+private:
     int maxDiameter;
-    
+
     int dfs(TreeNode* node) {
         if (!node) {
             return 0;
@@ -29,21 +31,36 @@
         // Return the depth of this subtree
         return max(leftDepth, rightDepth) + 1;
     }
-    
-    public:
+
+public:
     int diameterOfBinaryTree(TreeNode* root) {
         maxDiameter = 0;
         dfs(root);
         return maxDiameter;
     }
- };
- int main() {
+};
+
+int main() {
     Solution solution;
+    // Build tree:       1
+    //                  / \
+    //                 2   3
+    //                /
+    //               4
+    // Diameter: path 4->2->1->3 = 3 edges
     TreeNode* root = new TreeNode(1);
     root->left = new TreeNode(2);
     root->right = new TreeNode(3);
     root->left->left = new TreeNode(4);
-    root->left->right = new TreeNode(5);
-    cout << solution.diameterOfBinaryTree(root) << endl;
+
+    int result = solution.diameterOfBinaryTree(root);
+    cout << "Diameter: " << result << endl;  // Expected: 3 (path 4-2-1-3)
+
+    // Cleanup
+    delete root->left->left;
+    delete root->left;
+    delete root->right;
+    delete root;
+
     return 0;
- }
+}
